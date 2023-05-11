5652e1b6eeab8c68f23925cfa9efb4e3c87f2fc5
daniel-web-developer <daniel_caixeta@protonmail.com>
2023-05-11 23:04:55 +0200

    finally done!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import imgDesktop from './images/image-product-desktop.jpg';
+import imgMobile from './images/image-product-mobile.jpg';
+import iconCart from './images/icon-cart.svg';
+
+import './style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-justcont-c flex-alignitems-c main">
+      <div className="flex main-flex main-box">
+        <img src={imgDesktop} alt="" className="main-img none-mobile"></img>  
+        <img src={imgMobile} alt="" className="main-img none-desktop"></img>
+        <div className="text">
+          <p className="text-tag">PERFUME</p>
+          <p className="text-title">Gabrielle Essence Eau<br className="none-mobile"></br> De Parfum</p>
+          <p className="text-desc">A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
+          <div className="flex flex-alignitems-c text-prices">
+            <p className="text-price">$149.99</p>
+            <p className="text-real"><del>$169.99</del></p>
+          </div>
+          <a href="#"><div className="flex flex-justcont-c flex-alignitems-c button">
+            <img src={iconCart} alt=""></img>
+            <p>Add to Cart</p>
+          </div></a>
+        </div>
+      </div>
     </div>
   );
 }
